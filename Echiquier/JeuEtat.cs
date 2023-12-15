f78878c39e46a31f3b92797a49e4269ae39f9313
Flykerz <schrivetheo@gmail.com>
2023-12-15 18:32:42 +0100

    cleanup

--- Echiquier/JeuEtat.cs
+++ Echiquier/JeuEtat.cs
@@ -15,9 +15,10 @@ namespace Echiquier
         public const int DIMENSION = 10;
 
         private readonly CaseEtat[] _cases;
-        // private readonly Plateau _plateau;
 
-        private bool joueurEnCours = false; // Commence par blanc
+        private List<CaseEtat> _mangerObligatoires = new List<CaseEtat>();
+
+        private bool _joueurEnCours = false; // Commence par blanc
 
         public JeuEtat()
         {
@@ -32,55 +33,53 @@ namespace Echiquier
                     PieceInitiale(i)
                 );
             }
-
-            // this._plateau = new Plateau(this._cases);
         }
 
         private PieceEtat? PieceInitiale(int pos)
         {
             switch (pos)
             {
-                    case 1: return new PieceEtat(new PionType(), true);
-                    case 3: return new PieceEtat(new PionType(), true);
-                    case 5: return new PieceEtat(new PionType(), true);
-                    case 7: return new PieceEtat(new PionType(), true);
-                    case 9: return new PieceEtat(new PionType(), true);
-                    case 10: return new PieceEtat(new PionType(), true);
-                    case 12: return new PieceEtat(new PionType(), true);
-                    case 14: return new PieceEtat(new PionType(), true);
-                    case 16: return new PieceEtat(new PionType(), true);
-                    case 18: return new PieceEtat(new PionType(), true);
-                    case 21: return new PieceEtat(new PionType(), true);
-                    case 23: return new PieceEtat(new PionType(), true);
-                    case 25: return new PieceEtat(new PionType(), true);
-                    case 27: return new PieceEtat(new PionType(), true);
-                    case 29: return new PieceEtat(new PionType(), true);
-                    case 30: return new PieceEtat(new PionType(), true);
-                    case 32: return new PieceEtat(new PionType(), true);
-                    case 34: return new PieceEtat(new PionType(), true);
-                    case 36: return new PieceEtat(new PionType(), true);
-                    case 38: return new PieceEtat(new PionType(), true);
+                case 1: return new PieceEtat(new PionType(), true);
+                case 3: return new PieceEtat(new PionType(), true);
+                case 5: return new PieceEtat(new PionType(), true);
+                case 7: return new PieceEtat(new PionType(), true);
+                case 9: return new PieceEtat(new PionType(), true);
+                case 10: return new PieceEtat(new PionType(), true);
+                case 12: return new PieceEtat(new PionType(), true);
+                case 14: return new PieceEtat(new PionType(), true);
+                case 16: return new PieceEtat(new PionType(), true);
+                case 18: return new PieceEtat(new PionType(), true);
+                case 21: return new PieceEtat(new PionType(), true);
+                case 23: return new PieceEtat(new PionType(), true);
+                case 25: return new PieceEtat(new PionType(), true);
+                case 27: return new PieceEtat(new PionType(), true);
+                case 29: return new PieceEtat(new PionType(), true);
+                case 30: return new PieceEtat(new PionType(), true);
+                case 32: return new PieceEtat(new PionType(), true);
+                case 34: return new PieceEtat(new PionType(), true);
+                case 36: return new PieceEtat(new PionType(), true);
+                case 38: return new PieceEtat(new PionType(), true);
 
-                    case 61: return new PieceEtat(new PionType(), false);
-                    case 63: return new PieceEtat(new PionType(), false);
-                    case 65: return new PieceEtat(new PionType(), false);
-                    case 67: return new PieceEtat(new PionType(), false);
-                    case 69: return new PieceEtat(new PionType(), false);
-                    case 70: return new PieceEtat(new PionType(), false);
-                    case 72: return new PieceEtat(new PionType(), false);
-                    case 74: return new PieceEtat(new PionType(), false);
-                    case 76: return new PieceEtat(new PionType(), false);
-                    case 78: return new PieceEtat(new PionType(), false);
-                    case 81: return new PieceEtat(new PionType(), false);
-                    case 83: return new PieceEtat(new PionType(), false);
-                    case 85: return new PieceEtat(new PionType(), false);
-                    case 87: return new PieceEtat(new PionType(), false);
-                    case 89: return new PieceEtat(new PionType(), false);
-                    case 90: return new PieceEtat(new PionType(), false);  
-                    case 92: return new PieceEtat(new PionType(), false);
-                    case 94: return new PieceEtat(new PionType(), false);
-                    case 96: return new PieceEtat(new PionType(), false);
-                    case 98: return new PieceEtat(new PionType(), false);
+                case 61: return new PieceEtat(new PionType(), false);
+                case 63: return new PieceEtat(new PionType(), false);
+                case 65: return new PieceEtat(new PionType(), false);
+                case 67: return new PieceEtat(new PionType(), false);
+                case 69: return new PieceEtat(new PionType(), false);
+                case 70: return new PieceEtat(new PionType(), false);
+                case 72: return new PieceEtat(new PionType(), false);
+                case 74: return new PieceEtat(new PionType(), false);
+                case 76: return new PieceEtat(new PionType(), false);
+                case 78: return new PieceEtat(new PionType(), false);
+                case 81: return new PieceEtat(new PionType(), false);
+                case 83: return new PieceEtat(new PionType(), false);
+                case 85: return new PieceEtat(new PionType(), false);
+                case 87: return new PieceEtat(new PionType(), false);
+                case 89: return new PieceEtat(new PionType(), false);
+                case 90: return new PieceEtat(new PionType(), false);
+                case 92: return new PieceEtat(new PionType(), false);
+                case 94: return new PieceEtat(new PionType(), false);
+                case 96: return new PieceEtat(new PionType(), false);
+                case 98: return new PieceEtat(new PionType(), false);
 
                 default: return null;
             }
@@ -92,68 +91,65 @@ namespace Echiquier
         }
 
         // Déclenché lors du clic sur une case.
-        public void Interaction(CaseEtat c)
+        public void Interaction(CaseEtat caseCliquee)
         {
-            CaseEtat source = null;
+            // If no source is selected, select the current case if it belongs to the current player
+            CaseEtat? source = GetCaseSelectionnee();
+            if (source == null)
+            {
+                if (caseCliquee.Piece != null && caseCliquee.Piece.Couleur == _joueurEnCours)
+                {
+                    caseCliquee.Selectionne(_joueurEnCours);
+                }
+                return;
+            }
 
+            // Si on a une source et une destination de définie
+            CaseEtat? ennemiAManger = MouvementManger(source, caseCliquee);
+            if (ennemiAManger != null)
+            {
+                source.Mouvement(caseCliquee);
+                ennemiAManger.MangePiece();
+                ChangeJoueur();
+            }
+            else if (MouvementSimple(source, caseCliquee))
+            {
+                source.Mouvement(caseCliquee);
+                ChangeJoueur();
+
+                int gagne = Gagne(caseCliquee);
+                if (gagne == 1)
+                {
+                    MessageBox.Show("Les Blancs ont gagnés");
+                }
+                else if (gagne == 2)
+                {
+                    MessageBox.Show("Les Noirs ont gagnés");
+                }
+            }
+            else
+            {
+                caseCliquee.Deselectionne();
+            }
+        }
+
+        private CaseEtat? GetCaseSelectionnee()
+        {
             // Find the selected source case
             for (int i = 0; i < _cases.Length; i++)
             {
                 if (_cases[i].Selection)
                 {
-                    source = _cases[i];
-                    break;
+                    return _cases[i];
                 }
             }
 
-            // If no source is selected, select the current case if it belongs to the current player
-            if (source == null)
-            {
-                if (c.Piece != null && c.Piece.Couleur == joueurEnCours)
-                {
-                    c.Selectionne(joueurEnCours);
-                }
-            }
-            else
-            {
-                CaseEtat? ennemiAManger = MouvementManger(source, c);
-                if (ennemiAManger != null)
-                {
-                    source.Mouvement(c);
-                    ennemiAManger.MangePiece();
-                    joueurEnCours = !joueurEnCours;
+            return null;
+        }
 
-                    // int positionCaseEntre = (source.Position + c.Position) / 2;
-                    // _cases[positionCaseEntre].MangePiece();
-                    // source.Mouvement(c);
-                    // mange = true;
-
-
-
-                    //     c.Selectionne(joueurEnCours); // Si le pion a Manger, On lui laisse la possibilité de rejouer derrière
-
-
-                }
-                else if (MouvementSimple(source, c))
-                {
-                    source.Mouvement(c);
-                    joueurEnCours = !joueurEnCours;
-
-                    int gagne = Gagne(c);
-                    if (gagne == 1)
-                    {
-                        MessageBox.Show("Les Blancs ont gagnés");
-                    }
-                    else if (gagne == 2)
-                    {
-                        MessageBox.Show("Les Noirs ont gagnés");
-                    }
-                }
-                else
-                {
-                    c.Deselectionne();
-                }
-            }
+        private void ChangeJoueur()
+        {
+            _joueurEnCours = !_joueurEnCours;
         }
 
 
@@ -164,14 +160,14 @@ namespace Echiquier
 
             if (!destinationIsEmpty)
             {
-                return null; 
+                return null;
             }
 
             // Calcule la position des pieces entre source et target en fonction de la direction
             //Direction 1
             if (target.X == source.X + 2 && target.Y == source.Y - 2)
             {
-                int positionCaseEntreX = source.X + 1 ;
+                int positionCaseEntreX = source.X + 1;
                 int positionCaseEntreY = source.Y - 1;
                 int indexCaseEntre = positionCaseEntreY * DIMENSION + positionCaseEntreX;
                 CaseEtat entreCaseEtat = _cases[indexCaseEntre];
@@ -210,39 +206,11 @@ namespace Echiquier
                 return estEnnemi ? entreCaseEtat : null;
             }
 
-
-
-
-            // // Calcule la position des pieces entre source et target
-            // int positionCaseEntre = (source.Position + target.Position) / 2;
-
-            // // Verifie si il y a une piece sur la position entre source et target
-            // bool isPieceBetween = _cases[positionCaseEntre].Piece != null;
-
-            // if (isPieceBetween && _cases[positionCaseEntre].Piece.Couleur != source.Couleur)
-            // {
-            //     // Verifie si la position de target est diagonale de 2 cases
-            //     if (Math.Abs(target.Position - source.Position) == 2 * JeuEtat.DIMENSION + 2 ||
-            //         Math.Abs(target.Position - source.Position) == 2 * JeuEtat.DIMENSION - 2)
-            //     {
-            //         // Check if the position behind the enemy piece is empty
-            //         int positionBehindEnemy = (positionCaseEntre + source.Position) / 2;
-            //         bool isEmptyBehindEnemy = _cases[positionBehindEnemy].Piece == null;
-
-            //         if (isEmptyBehindEnemy)
-            //         {
-            //             // Remove the captured piece from the board
-            //             _cases[positionCaseEntre].MangePiece();
-            //             return true; // Return true if it's an opponent's piece and the target is two spaces ahead diagonally with an empty space behind the enemy
-            //         }
-            //     }
-            // }
-
             return null; // No valid capture or regular move found
         }
 
 
-      
+
         public bool MouvementSimple(CaseEtat source, CaseEtat target)
         {
             // On ne peut se déplacer que sur une case libre
@@ -265,27 +233,11 @@ namespace Echiquier
                 || (source.Piece.Couleur == true && target.Y == source.Y + 1 && target.X == source.X + 1)
                 || (source.Piece.Couleur == false && target.Y == source.Y - 1 && target.X == source.X - 1)
                 || (source.Piece.Couleur == false && target.Y == source.Y - 1 && target.X == source.X + 1);
+        }
 
-
-
-
-
-            // if (source.X % DIMENSION == 0)
-            // {
-            //     return (source.Piece.Couleur == true && target.Position == source.Position + DIMENSION + 1)
-            //         || (source.Piece.Couleur == false && target.Position == source.Position - DIMENSION + 1);
-
-            // }
-            // if (source.Position % DIMENSION == DIMENSION - 1)
-            // {
-            //     return (source.Piece.Couleur == true && target.Position == source.Position + DIMENSION - 1)
-            //         || (source.Piece.Couleur == false && target.Position == source.Position - DIMENSION - 1);
-
-            // }
-            // return (source.Piece.Couleur == true && target.Position == source.Position + DIMENSION - 1)
-            //         || (source.Piece.Couleur == true && target.Position == source.Position + DIMENSION + 1)
-            //         || (source.Piece.Couleur == false && target.Position == source.Position - DIMENSION - 1)
-            //         || (source.Piece.Couleur == false && target.Position == source.Position - DIMENSION + 1);
+        public List<CaseEtat> CalculeMangerObligatoires() 
+        {
+            return new List<CaseEtat>();
         }
 
         public int Gagne(CaseEtat source)
@@ -300,21 +252,9 @@ namespace Echiquier
                 return 2;
 
             }
-            else { return 0; }
-            
-            // if (source.Piece.Couleur == false && source.Position < DIMENSION )
-            // {
-            //     return 1;
-
-            // }
-            // if (source.Piece.Couleur == true && source.Position > DIMENSION * (DIMENSION - 1))
-            // {
-            //     return 2;
-
-            // }
-            // else { return  0; }
-
-
+            else {
+                return 0;
+            }
         }
     }
 }
